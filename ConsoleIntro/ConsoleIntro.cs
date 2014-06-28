e6db9c05d80b3e458a6e1062fe2ec76324ebd1c8
DarthXeth <xwaxman@gmail.com>
2014-06-28 17:31:47 -0600

    Finished Up v1.0
    Added final 3 modules, made shit understandabale. Jake is liking this,
    so I'ma keep going.

--- ConsoleIntro/ConsoleIntro.cs
+++ ConsoleIntro/ConsoleIntro.cs
@@ -17,7 +17,7 @@ using System.Text;
  *  4)  current date and time
  *  5)  Simple maths
  *  6)  sequential printing - really primitive logic/algorithm foo
- *  7)  more math + standard input foo
+ *  7)  more math + standard input foo + dates
  *  
  *  The idea for this (and the inspiration for a lot of these exercises) come from 
  *  "Fundamentals of Computer Programming with C#", Nakov, et. al., 2013. Very good book, and they deserve a lot 
@@ -62,10 +62,13 @@ namespace ConsoleIntro
                         runWhatTime();
                         break;
                     case 5:     //Math is easy
+                        runMath();
                         break;
                     case 6:     //Basic logic
+                        runLogic();
                         break;
                     case 7:     //changing variables
+                        runWorkWithDates();
                         break;
                     case 99:    //exit
                         keepRunning = false;
@@ -94,7 +97,7 @@ namespace ConsoleIntro
             menuOption[3] = "4) What time is it?";
             menuOption[4] = "5) Math is easy (for computers...)";
             menuOption[5] = "6) Basic logic";
-            menuOption[6] = "7) Changing variables";
+            menuOption[6] = "7) Work with Dates";
             menuOption[7] = "...or, press any other key to exit";
 
 
@@ -227,6 +230,163 @@ namespace ConsoleIntro
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
+        }
+
+        /// <summary>
+        /// Demonstrates basic math operations
+        /// </summary>
+        private static void runMath()
+        {
+            Console.Clear();
+
+            int x = 0, y = 0;
+            string xPick = "Pick an integer to assign to X: ";
+            string yPick = "Pick an integer to assign to Y: ";
+
+            bool validInput = false;
+
+            Console.WriteLine("C# is an excellent tool for math. For example, let's create two variables:");
+            
+            //keep looping until the x variable is filled
+            while (!validInput)
+            {
+                Console.WriteLine(xPick);
+                if (int.TryParse(Console.ReadLine(), out x))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid integer, please try again.");
+                }
+            }
+
+            validInput = false;
+            //keep looping until the y variable is filled
+            while (!validInput)
+            {
+                Console.WriteLine(yPick);
+                if (int.TryParse(Console.ReadLine(), out y))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid integer, please try again.");
+                }
+            }
+
+            //now we have an x and y
+            Console.WriteLine("To add, use the '+' sign: {0} + {1} = {2}", x, y, x + y);
+            Console.WriteLine("To subtract, use the '-' sign: {0} - {1} = {2}", x, y, x - y);
+            Console.WriteLine("To multiply, use the '*' sign: {0} * {1} = {2}", x, y, x * y);
+            Console.WriteLine("To divide, use the '/' sign (you only get the whole number, not the remainder: {0} / {1} = {2}"
+                , x, y, x / y);
+            Console.WriteLine("To get the remainder, use the '%' (modulus) sign: {0} % {1} = {2}",
+                x, y, x % y);
+            Console.WriteLine("So, {0} divided by {1} = {2} remainder {3}", x, y, x / y, x % y);
+            Console.WriteLine("There are a lot of other functions in the Math. library, such as squareroot, etc");
+            Console.WriteLine("The square root of {0} is Math.sqrt({0}) = {1}", x * y, Math.Sqrt(x * y));
+
+            closeModule();
+        }
+
+        /// <summary>
+        /// This method demonstrates algorithmic thinking. The goal is to find an algorithm to create the first 100
+        /// numbers of the following sequence: 1, -2, 3, -4, 5, etc.
+        /// </summary>
+        private static void runLogic()
+        {
+            Console.Clear();
+
+            //when looking at the sequence, we see a pattern - every even number is negative
+            //we can determine a number is even if it's modulus of 2 (n % 2) is 0
+            //therefore, we can simply loop from 1 through 100, modding by 2, and multiplying the
+            //result by -1 when appropriate
+            //since we want 5to print the whole sequence at once, we'll need to use a string type, and when
+            //working with Strings, StringBuilder is generally the best class to use.
+            StringBuilder output = new StringBuilder();
+            for (int i = 1; i <= 100; i++)
+            {
+                int x = i;
+                if (x % 2 == 0)
+                    x *= -1;
+                output.Append(x);
+
+                //we don't want to add a comma separator for the last number
+                if (i < 100)
+                    output.Append(", ");
+            }
+
+            Console.WriteLine(output);
+
+            closeModule();
+        }
+
+        /// <summary>
+        /// This demonstrates reading input and working with the result
+        /// </summary>
+        private static void runWorkWithDates()
+        {
+            Console.Clear();
+
+            string prompt = "Please enter your birthday in MM/DD/YYYY format: ";
+            bool validInput = false;
+            DateTime birthday = DateTime.Now;
+
+            while (!validInput)
+            {
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine();
+                if (!DateTime.TryParse(input, out birthday))
+                {
+                    Console.WriteLine("Invalid date or format");
+                }
+                else
+                {
+                    validInput = true;
+                }
+
+            }
+
+            //now determine how many years, months, days, hours, minutes, and seconds old you are
+            int years = DateTime.Now.Year - birthday.Year;
+            
+            //find the months...
+            int months = DateTime.Now.Month - birthday.Month;
+
+            //this heuristic corrects for the year wrap-around
+            if (months < 0)
+                months += 12;
+
+            //find the days...
+            int days = DateTime.Now.Day - birthday.Day;
+            //this corrects for days
+            if (days < 0)
+                days *= -1;
+
+            //find the hours
+            int hours = DateTime.Now.Hour - birthday.Hour;
+            //correct for hour wrapper
+            if (hours < 0)
+                hours += 24;
+
+            //find the minutes
+            int minutes = DateTime.Now.Minute - birthday.Minute;
+            //correct for 60 minute wrap-around
+            if (minutes < 0)
+                minutes += 60;
+
+            //find the seconds
+            int seconds = DateTime.Now.Second - birthday.Second;
+            //correct for 60 second wrap-around
+            if (seconds < 0)
+                seconds += 60;
+
+            Console.WriteLine("You are {0} years, {1} months, {2} days, {3} hours, {4} minutes, and {5} seconds old.",
+                years, months, days, hours, minutes, seconds);
+
+            closeModule();
         }
     }
 }
